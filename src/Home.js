dc778c9d78a86fae97a02dc9eed1a482ee847a49
Luka Leskovec <llesko00@fesb.hr>
2024-03-20 16:43:54 +0100

    Added a json file for data

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
 
-    const [blogs,setBlogs] = useState([
-        {title:"First blog", preview:"This is the first blog...", author:"Luka", id:0},
-        {title:"Austria trip", preview:"We set off on thursday with...", author:"Luka", id:1},
-        {title:"Tennis", preview:"The most amazing tennis court I have...", author:"Luka", id:2}
-    ]);
+    const [blogs,setBlogs] = useState(null);
 
     const handleDeleteBlog = (id) => {
         const newBlogs=blogs.filter((blog)=>blog.id!==id)
         setBlogs(newBlogs);
     }
 
+    useEffect(()=>{
+        fetch("http://localhost:8000/blogs")
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setBlogs(data);
+        })
+    },[]);
+
     return (
         <div className="home">
             <div className="home-content">
-                <BlogList blogs={blogs} title="All blogs:" handleDeleteBlog={handleDeleteBlog}/>
+                {blogs && <BlogList blogs={blogs} title="All blogs:" handleDeleteBlog={handleDeleteBlog}/>}
             </div>
         </div>
     );
