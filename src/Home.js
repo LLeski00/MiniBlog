8620ba16271b247e3ac56d94b87a3da1823ea56d
Luka Leskovec <llesko00@fesb.hr>
2024-03-20 20:25:07 +0100

    Error handling

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,7 @@ const Home = () => {
 
     const [blogs,setBlogs] = useState(null);
     const [isLoading,setIsLoading] = useState(true);
+    const [error,setError] = useState(null);
 
     const handleDeleteBlog = (id) => {
         const newBlogs=blogs.filter((blog)=>blog.id!==id)
@@ -14,10 +15,18 @@ const Home = () => {
     useEffect(()=>{
         fetch("http://localhost:8000/blogs")
         .then(res => {
+            if(!res.ok){
+                throw Error("Could not fetch the data.");
+            }
             return res.json();
         })
         .then(data => {
             setBlogs(data);
+            setIsLoading(false);
+            setError(null);
+        })
+        .catch(err => {
+            setError(err.message);
             setIsLoading(false);
         })
     },[]);
@@ -25,6 +34,7 @@ const Home = () => {
     return (
         <div className="home">
             <div className="home-content">
+                {error && <p className="blog-error">{error}</p>}
                 {isLoading && <p className="blog-loading">Loading...</p>}
                 {blogs && <BlogList blogs={blogs} title="All blogs:" handleDeleteBlog={handleDeleteBlog}/>}
             </div>
