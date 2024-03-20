285705b7449937e5b54f8d9fc1ceec9042ae2325
Luka Leskovec <llesko00@fesb.hr>
2024-03-20 21:50:30 +0100

    Added a Create component

---
+++ src/Create.js
@@ -0,0 +1,11 @@
+const Create = () => {
+    return (
+        <div className="create">
+            <div className="create-content">
+                <h1>Add a new blog:</h1>
+            </div>
+        </div>
+    );
+}
+ 
+export default Create;
