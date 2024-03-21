5302b6f77e9d834350e29b024725a649c6423039
Luka Leskovec <llesko00@fesb.hr>
2024-03-21 19:53:56 +0100

    Outputting content of a blog on the blog details page

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,13 +1,23 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
 
     const {id}=useParams();
+    const {info:blog,isLoading,error} = useFetch("http://localhost:8000/blogs/"+id);
 
     return (
         <div className="blog-details">
             <div className="blog-details-content">
                 <h1>Blog No.{id} details: </h1>
+                {error && <p className="blog-error">{error}</p>}
+                {isLoading && <p className="blog-loading">Loading...</p>}
+                {blog &&
+                    <div className="blog-details-data">
+                        <p className="blog-details-body">{blog.body}</p>
+                        <p className="blog-details-author">Written by: {blog.author}</p>
+                    </div>
+                }
             </div>
         </div>
     );
