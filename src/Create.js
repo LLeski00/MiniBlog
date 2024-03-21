d7f8e1eba9d1fa697b4f8dadb44b1ad95799545a
Luka Leskovec <llesko00@fesb.hr>
2024-03-21 20:53:44 +0100

    Added a form in the Create page

--- src/Create.js
+++ src/Create.js
@@ -1,8 +1,43 @@
+import { useState } from "react";
+
 const Create = () => {
+
+    const [title,setTitle] = useState("");
+    const [body,setBody] = useState("");
+    const [author,setAuthor] = useState("");
+
+    const handleBlogSubmit = (e) => {
+        e.preventDefault();
+        const preview = body.slice(0,20)+"...";
+        const blogContent = {title,preview,body,author};
+        console.log(blogContent);
+    }
+
     return (
         <div className="create">
             <div className="create-content">
                 <h1>Add a new blog:</h1>
+
+                <form onSubmit={handleBlogSubmit}>
+                    <label>Blog title:</label>
+                    <input 
+                        type="text" 
+                        required 
+                        value={title}
+                        onChange={(e)=>setTitle(e.target.value)}/>
+                    <label>Blog body:</label>
+                    <textarea 
+                        required 
+                        value={body}
+                        onChange={(e)=>setBody(e.target.value)}></textarea>
+                    <label>Blog author:</label>
+                    <input 
+                        type="text" 
+                        required 
+                        value={author}
+                        onChange={(e)=>setAuthor(e.target.value)}/>
+                    <button>Add blog</button>
+                </form>
             </div>
         </div>
     );
