19544e3bd579af5c1eb82bd06f04a6413e6dacbf
Luka Leskovec <llesko00@fesb.hr>
2024-03-21 21:41:26 +0100

    Added a delete blog button and a not found page

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,20 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
 
     const {id}=useParams();
     const {info:blog,isLoading,error} = useFetch("http://localhost:8000/blogs/"+id);
+    const navigate=useNavigate();
+
+    const handleBlogDelete = () => {
+        fetch("http://localhost:8000/blogs/" + blog.id, {
+            method: 'DELETE'
+        })
+        .then(()=>{
+            navigate("/");
+        })  
+    }
 
     return (
         <div className="blog-details">
@@ -16,6 +26,7 @@ const BlogDetails = () => {
                         <h1 className="blog-details-title">{blog.title}</h1>
                         <h2 className="blog-details-author">By: {blog.author}</h2>
                         <p className="blog-details-body">{blog.body}</p>
+                        <button onClick={handleBlogDelete}>Delete blog</button>
                     </div>
                 }
             </div>
---
+++ src/NotFound.js
@@ -0,0 +1,12 @@
+const NotFound = () => {
+    return (
+        <div className="not-found">
+            <div className="not-found-content">
+                <h1>Sorry :/</h1>
+                <p>The page that you are looking for doesn't exist.</p>
+            </div>
+        </div>
+    );
+}
+ 
+export default NotFound;
